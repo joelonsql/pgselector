50fa9cae5c642a0e5c1a5e4ea6458225f4a8672d
Joel Jacobson <joel@trustly.com>
2016-12-18 21:47:39 +0700

    Separate HTML from Perl and put into separate file, HTML.pm

--- HTML.pm
+++ HTML.pm
@@ -19,49 +19,58 @@ sub Hash2Query {
 
 sub HyperLink {
     my ($Self, $Attr) = @_;
-    return qq!<a href="$Attr->{HREF}">$Attr->{Text}</a>!;
+    return qq!<a href="?$Attr->{HREF}">$Attr->{Text}</a>\n!;
 }
 
 sub NodeLink {
     my ($Self, $Attr) = @_;
+    my $Node;
+    my $UpDownArrow;
+    if (defined($Attr->{Parent})) {
+        $Node = $Attr->{Parent};
+        $UpDownArrow = '&uarr;';
+    } elsif (defined($Attr->{Child})) {
+        $Node = $Attr->{Child};
+        $UpDownArrow = '&darr;';
+    }
     my $HREF = $Self->Hash2Query({
-        FilterSchema          => $Attr->{Node}->{schema},
-        FilterTable           => $Attr->{Node}->{table},
-        FilterColumn          => $Attr->{Node}->{column},
-        FilterValue           => $Attr->{Node}->{value},
-        ShowUniqueNamesForIDs => $Attr->{ShowUniqueNamesForIDs}
+        FilterSchema          => $Node->{schema},
+        FilterTable           => $Node->{table},
+        FilterColumn          => $Node->{column},
+        FilterValue           => $Node->{value},
+        ShowUniqueNamesForIDs => $Self->{Params}->{ShowUniqueNamesForIDs}
     });
     if ($Attr->{Type} eq 'TABLE') {
         return $Self->HyperLink({
             HREF  => $HREF,
-            Text => ($Attr->{Node}->{schema} eq 'public' ? '' : $Attr->{Node}->{schema} . '.') . $Attr->{Node}->{table}
+            Text => ($Node->{schema} eq 'public' ? '' : $Node->{schema} . '.') . $Node->{table}
         });
-    } elsif ($Attr->{Type} eq 'VALUE' && $Attr->{ShowUniqueNamesForIDs} eq '1' && defined($Attr->{Node}->{label})) {
+    } elsif ($Attr->{Type} eq 'VALUE' && $Self->{Params}->{ShowUniqueNamesForIDs} eq '1' && defined($Node->{label})) {
         return $Self->HyperLink({
             HREF  => $HREF,
-            Text => $Attr->{Node}->{label}
+            Text => $Node->{label}
         });
     }
     return $Self->HyperLink({
-        HREF  => $HREF,
-        Text => $Attr->{Node}->{value}
+        HREF => $HREF,
+        Text => $UpDownArrow . $Node->{value}
     });
 }
 
 sub Select {
     my ($Self, $Attr) = @_;
     my $HTML = qq!
-        <select name="$Attr->{Name}" ! . ($Attr->{OnChangeSubmit} ? qq! onchange="this.form.submit()"! : '') . qq!>
+        <select name="$Attr->{Name}" ! . ($Attr->{OnChangeSubmit} ? qq! onchange="document.getElementById('$Self->{FormID}').submit();"! : '') . qq!>
             <option value="$Attr->{DefaultValue}">$Attr->{DefaultText}</option>
     !;
     foreach my $Value (@{$Attr->{Options}}) {
         if ($Attr->{SelectedValue} eq $Value) {
-            $HTML .= qq!<option selected="selected">$Value</option>!;
+            $HTML .= qq!<option selected="selected">$Value</option>\n!;
         } else {
-            $HTML .= qq!<option>$Value</option>!;
+            $HTML .= qq!<option>$Value</option>\n!;
         }
     }
-    $HTML .= qq!</select>!;
+    $HTML .= qq!</select>\n!;
     return $HTML;
 }
 
@@ -71,50 +80,107 @@ sub Input {
 
     my $Dec;
     my $Inc;
-    if (defined($Params->{Increment}) && $Attr->{Value} = ~ m/^\d+$/) {
-        $Dec = ($Attr->{Value} - $Params->{Increment});
-        if ($Dec < $Attr->{Min}) {
+    if (defined($Attr->{Increment}) && $Attr->{Value} =~ m/^\d+$/) {
+        $Dec = ($Attr->{Value} - $Attr->{Increment});
+        if (defined($Attr->{Min}) && $Dec < $Attr->{Min}) {
             $Dec = undef;
         }
-        $Inc = ($Attr->{Value} + $Params->{Increment});
-        if ($Inc > $Attr->{Max}) {
+        $Inc = ($Attr->{Value} + $Attr->{Increment});
+        if (defined($Attr->{Max}) && $Inc > $Attr->{Max}) {
             $Inc = undef;
         }
     }
     if (defined($Dec)) {
-        $HTML .= qq!<input type="button" onClick="document.getElementById('$Attr->{Name}').value = '$Dec'; document.getElementById('$Self->{FormID}').submit();" value="$Dec"/>!;
+        $HTML .= qq!<input type="button" onClick="document.getElementById('$Attr->{Name}').value = '$Dec'; document.getElementById('$Self->{FormID}').submit();" value="$Dec"/>\n!;
     }
-    $HTML .= qq!<input id="$Attr->{Name}" name="$Attr->{Name}" value="$Attr->{Value}"/>!;
+    $HTML .= qq!<input id="$Attr->{Name}" name="$Attr->{Name}" value="$Attr->{Value}"/>\n!;
     if (defined($Inc)) {
-        $HTML .= qq!<input type="button" onClick="document.getElementById('$Attr->{Name}').value = '$Inc'; document.getElementById('$Self->{FormID}').submit();" value="$Inc"/>!;
+        $HTML .= qq!<input type="button" onClick="document.getElementById('$Attr->{Name}').value = '$Inc'; document.getElementById('$Self->{FormID}').submit();" value="$Inc"/>\n!;
     }
     return $HTML;
 }
 
-sub Cell {
-    my ($Self, $Attr) = @_;
-    my @Classes = ('cell');
-    my $Text    = $Attr->{Value};
+sub Table {
+    my ($Self, $Rows) = @_;
     my $HTML = '';
+    $HTML .= qq!<div class="table">\n!;
+    my $HeaderRow = shift @$Rows;
+    use Data::Dumper;
+    print Dumper $HeaderRow;
+    $HTML .= $Self->RowHeader(join('',@$HeaderRow));
+    foreach my $Row (@$Rows) {
+        $HTML .= $Self->Row(join('',@$Row));
+    }
+    $HTML .= qq!</div>\n!;
+    return $HTML;
+}
+
+sub Row {
+    my ($Self, $InnerHTML) = @_;
+    return qq!<div class="row">$InnerHTML</div>\n!;
+}
+
+sub RowHeader {
+    my ($Self, $InnerHTML) = @_;
+    return qq!<div class="row header blue">$InnerHTML</div>\n!;
+}
+
+sub Cell {
+    my ($Self, $Attr, $Classes) = @_;
+    my $Column   = $Attr->{Column};
+    my $Value    = $Attr->{Value};
+    my $Classes  = $Attr->{Classes};
+    push @$Classes, 'cell';
+    my $HTML = '';
+    if ($Self->{Params}->{FilterColumn} eq $Column
+    &&  $Self->{Params}->{FilterValue}  eq $Value) {
+        push @$Classes, 'blink_me';
+    }
+    $HTML .= qq!<div class="! . join(' ',@$Classes) . qq!">$Value</div>\n!;
+    return $HTML;
+}
+
+sub Collapse {
+    my ($Self, $Attr) = @_;
+    my $ID = ++$Self->{CollapseCellID};
+    my $HTML = '';
+    $HTML .= qq!<label class="collapse link" for="cell-$ID">$Attr->{Label}</label>\n!;
+    $HTML .= qq!<input id="cell-$ID" type="checkbox" checked/>\n!;
+    $HTML .= qq!<div>\n$Attr->{InnerHTML}\n</div>\n!;
+    return $HTML;
+}
+
+sub TextCell {
+    my ($Self, $Attr) = @_;
+    my $Text = $Attr->{Value};
+    my $Classes = [];
     if (!defined($Text)) {
-        push @Classes, 'null';
+        push @$Classes, 'null';
         $Text = 'NULL';
     } elsif (length($Text) > 80) {
         $Text =~ s!^(.{40}).*?(.{40})$!$1...$2!;
-        push @Classes, 'truncated';
+        push @$Classes, 'truncated';
     }
-    $HTML .= qq!<div class="! . join(' ',@Classes) . qq!">$Text</div>!;
-    return $HTML;
+    return $Self->Cell({Column => $Attr->{Column}, Value => $Text, Classes => $Classes});
+}
+
+sub Cells {
+    my ($Self, $Values) = @_;
+    my $Cells = [];
+    foreach my $Value (@$Values) {
+        push @$Cells, $Self->Cell({Value => $Value});
+    }
+    return $Cells;
 }
 
 sub Submit {
     my ($Self, $Attr) = @_;
-    return qq!<input type="submit" value="$Attr->{Value}"/>!;
+    return qq!<input type="submit" value="$Attr->{Value}"/>\n!;
 }
 
 sub Body {
     my ($Self, $InnerHTML) = @_;
-    return qq!<body translate="no">$InnerHTML</form>!;
+    return qq!<body translate="no">$InnerHTML</form>\n!;
 }
 
 sub Head {
@@ -130,29 +196,39 @@ sub Head {
 
 sub HTML {
     my ($Self, $InnerHTML) = @_;
-    return qq!<html>$InnerHTML</html>!;
+    return qq!<html>\n$InnerHTML\n</html>!;
 }
 
 sub Form {
-    my ($Self, $Attr, $InnerHTML) = @_;
-    $Self->{FormID} = $Attr->{ID};    
-    return qq!<form id="$Attr->{ID}">$InnerHTML</form>!;
+    my ($Self, $InnerHTML) = @_;
+    return qq!<form id="$Self->{FormID}">\n$InnerHTML\n</form>\n!;
 }
 
 sub ResetButton {
     my ($Self, $Attr) = @_;
     my $HTML = '';
-    $HTML .= qq!<input type="button" onClick="document.getElementById('$Attr->{Name}').value = ''; document.getElementById('$Self->{FormID}').submit();" value="$Attr->{Text}"/>!;
+    $HTML .= qq!<input type="button" onClick="document.getElementById('$Attr->{Name}').value = ''; document.getElementById('$Self->{FormID}').submit();" value="$Attr->{Text}"/>\n!;
 }
 
 sub ToggleButton {
     my ($Self, $Attr) = @_;
     my $HTML = '';
     my $CurValue   = $Self->{Params}->{$Attr->{Name}} || '0';
-    my $InvValue   = $CurValue eq '0' ? '1' : '0';
-    my $ButtonText = $CurValue eq '0' ? $Attr->{OnButton} : $Attr->{OffButton};
-    $HTML .= qq!<input type="hidden" name="$Attr->{Name}" id="$Attr->{Name}" value="$CurValue" />!;
-    $HTML .= qq!<input type="button" onClick="document.getElementById('$Attr->{Name}').value = '$InvValue'; document.getElementById('$Self->{FormID}').submit();" value="$ButtonText"/>!;
+    my $InvValue   = $CurValue eq '1' ? '0' : '1';
+    my $ButtonText = $CurValue eq '1' ? $Attr->{OnButton} : $Attr->{OffButton};
+    $HTML .= qq!<input type="hidden" name="$Attr->{Name}" id="$Attr->{Name}" value="$CurValue" />\n!;
+    $HTML .= qq!<input type="button" onClick="document.getElementById('$Attr->{Name}').value = '$InvValue'; document.getElementById('$Self->{FormID}').submit();" value="$ButtonText"/>\n!;
+    return $HTML;
+}
+
+sub Menu {
+    my ($Self, $Elements) = @_;
+    my $HTML = '';
+    $HTML .= qq!<div class="menu">\n!;
+    foreach my $InnerHTML (@$Elements) {
+        $HTML .= qq!<div class="cell">\n$InnerHTML\n</div>\n!;
+    }
+    $HTML .= qq!</div>\n!;
     return $HTML;
 }
 
